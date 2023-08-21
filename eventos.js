a28d1c5fa641e935fc9d6c2d82482ddd4efd0c58
MicaVia <micaela.viazzi@gmail.com>
2023-08-20 22:22:28 -0300

    solucion click

--- eventos.js
+++ eventos.js
@@ -1,13 +1,29 @@
-//const divi = document.querySelector('div');
-
-function mensaje() {
+function mensajeDiv() {
     alert ("Hola! Soy el div");
 };
 
+function mensajeBtn (event) {
+    alert('Hola!');
+    event.stopPropagation();
+}
 
-document.addEventListener('DOMContentLoaded', function() {
-    const divi = document.getElementById('divi');
-    divi.addEventListener('click', mensaje);
-})
+document.addEventListener ('DOMContentLoaded', () => {
+    const boton = document.querySelector('button');
+    
+    boton.addEventListener('click', mensajeBtn);
+});
+
+document.addEventListener ('DOMContentLoaded', () => { 
+    const divi = document.querySelector('div');
+    divi.addEventListener('click', mensajeDiv);
+});
+
 
 //divi.addEventListener ("click", alert ("Hola! Soy el div"));
+
+/*document.addEventListener('DOMContentLoaded', ()=> {
+   const botonn = document.getElementsByTagName('button');
+   for (let i=0; i<botonn.length; i++) {
+    botonn[i].addEventListener('click', alert('Hola!')); 
+   }
+});*/
